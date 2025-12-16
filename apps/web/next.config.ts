f203e63c5f98463f9f97f8eb50d21001a69c04b7
rowneth <wadroneth@gmail.com>
2025-12-16 22:10:34 +0530

    Configure allowed image domains in next.config.ts

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -3,6 +3,26 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh5.googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.squarespace-cdn.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'viewcy-heroku-production.s3.amazonaws.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
